3feeefeea06529cef356261f3067c830e45a3292
Webberdoo <freddie@webberdoo.co.uk>
2025-10-12 16:06:14 +0100

    feat: Move public assets to bundle root and include built assets

--- assets/vite.config.js
+++ assets/vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../src/Resources/public',
+    outDir: '../public',
     emptyOutDir: true,
     manifest: true,
     rollupOptions: {
