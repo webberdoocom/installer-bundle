19205075b9931e4c24da0dc231419bfbb5162da3
Webberdoo <freddie@webberdoo.co.uk>
2025-10-12 18:58:29 +0100

    fix: Add favicon back to public directory

--- assets/src/main.jsx
+++ assets/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './app.css';
+import './favicon.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('installer-root'));
 root.render(
