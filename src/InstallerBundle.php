131958b932900882a35bca12d175ae57dfffa64d
Webberdoo <freddie@webberdoo.co.uk>
2025-10-12 15:03:55 +0100

    fix: Properly load Extension and services in AbstractBundle

--- src/InstallerBundle.php
+++ src/InstallerBundle.php
@@ -2,12 +2,19 @@
 
 namespace Webberdoo\InstallerBundle;
 
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Webberdoo\InstallerBundle\DependencyInjection\InstallerExtension;
 
 class InstallerBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // Load services configuration
+        $container->import('../Resources/config/services.yaml');
+    }
+
     public function loadRoutes(ContainerConfigurator $configurator): void
     {
         $configurator->import(__DIR__ . '/Controller/', 'attribute');
@@ -16,5 +23,10 @@ class InstallerBundle extends AbstractBundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ?InstallerExtension
+    {
+        return new InstallerExtension();
     }
 }
